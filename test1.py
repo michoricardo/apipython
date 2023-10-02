1be8c67166f590978f04ed5666565b94f25a027f
Michoricardo <ricardog94@hotmail.com>
2023-10-02 16:53:50 -0600

    commit v2

--- test1.py
+++ test1.py
@@ -7,14 +7,18 @@ def elegirProductosPares():
             print(f"ID: {producto['id']}, Title: {producto['title']}, Price: ${producto['price']}")
 
 def altaUsuarioNuevo():
-    name = input("Ingrese el nombre del usuario: ")
-    username = input("Ingrese el nombre de usuario: ")
-    email = input("Ingrese el correo electrónico del usuario: ")
+    email = input("Ingrese su correo electronico: ")
+    username = input("Ingrese un nombre de usuario: ")
+    password = input("Ingrese un password: ")
+    firstname = input ("Ingrese su nombre sin apellido")
+    lastname = input ("Ingrese su apellido")
+
     
     payload = {
-        "name": name,
+        "email": email,
         "username": username,
-        "email": email
+        "password": password
+
     }
     
     response = requests.post("https://fakestoreapi.com/users", json=payload)
@@ -23,35 +27,37 @@ def altaUsuarioNuevo():
     print("Respuesta del servidor:", user_data)
 
 def imprimeCarrito():
+#Imprimir username, email (del cliente), title, price (del producto)
     global UID
     global PID
-    #Imprimir username, email (del cliente), title, price (del producto)
     response = requests.get("https://fakestoreapi.com/carts")
     carts = response.json()
-    print(carts) #Todos los carritos
+    #print(carts) #Todos los carritos
     for carrito in carts:
         UID = carrito['userId']
-        print("checando el UserName e Email del UID: ...", UID)
+        id = carrito['id']
+        print("Carrito con ID: ", id) #Para verificar que si se repite el usuario, sea diferente id de carrito
         consultaNameEmail(UID)
         prodsArray = carrito['products']
-        print(prodsArray)
+        #print(prodsArray)
         for prod in prodsArray:
             PID = prod['productId'] #Product ID de cada producto
             QTY = prod['quantity'] #cantidad de cada producto
             tituloPrecioProducto(PID,QTY)
         print("------------------------------------------\n")
 
-
 def consultaNameEmail(userID):
+    #Para extraer el nombre y el email de un usuario asociado a un carrito
     response = requests.get("https://fakestoreapi.com/users/"+str(userID))
-    userObject = response.json()
+    userObject = response.json() #Objeto respuesta usando el endpoint de un usuario especifico
     nombre = userObject['username']
     email = userObject['email']
     print("Nombre: "+ nombre,"| Email: ",email)
 
 def tituloPrecioProducto(productID,QTY):
+    #Para extraer el titulo y precio de un producto asociado a un carrito
     response = requests.get("https://fakestoreapi.com/products/"+str(productID))
-    prodObject = response.json()
+    prodObject = response.json() #objeto respuesta usando el endpoint de un producto especifico
     title = prodObject['title']
     price = prodObject['price']
     print("Producto:",title, " | Precio:", price, " | Cantidad: ",QTY)
@@ -59,6 +65,6 @@ def tituloPrecioProducto(productID,QTY):
 
 
 #elegirProductosPares()
-#altaUsuarioNuevo()
-imprimeCarrito()
+altaUsuarioNuevo()
+#imprimeCarrito()
 
